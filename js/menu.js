4cae69aff065ab39aa32273b4f0b109d3d0c9fad
Julia Vallina <soyjulis@gmail.com>
2018-07-02 12:58:05 +0200

    Fix indentation in menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 $(document).on('click', '.btn-mobile-menu', function(e) {
-    e.preventDefault();
+  e.preventDefault();
 	$(this).toggleClass('active');
 	$('.site-nav').toggleClass('active');
 });
